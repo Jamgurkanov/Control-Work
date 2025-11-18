3779ae09e419b3f0de60edc6062cd91fa2247cac
Senior_Lucas <zamgyrkanovmukas@gmail.com>
2025-11-18 14:35:58 +0600

    fist commit

--- homeworks/homework3.py
+++ homeworks/homework3.py
@@ -52,3 +52,57 @@ person3.introduce()
 cl1 = Classmate("Иван", "20.02.2000", "студент", True, "11D")
 cl1.introduce()
 fr1 = Friend("Айбек", "20.02.2000", "студент", True, "футбол")
+
+
+class Contact:
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+
+    @staticmethod
+    def validate_phone_number(phone_number):
+        return len(str(phone_number)) == 10 and str(phone_number).isdigit()
+
+
+class ContactList:
+    all_contacts = []
+
+    @classmethod
+    def add_contact(cls, name, phone_number):
+        if Contact.validate_phone_number(phone_number):
+            new_contact = Contact(name, phone_number)
+            cls.all_contacts.append(new_contact)
+            print(f"Контакт '{name}' успешно добавлен.")
+        else:
+            raise ValueError("Некорректный номер телефона. Номер должен содержать ровно 10 цифр.")
+
+try:
+    ContactList.add_contact("Иван", "1234567890")
+except ValueError as e:
+    print(e)
+
+try:
+    ContactList.add_contact("Петр", "123")
+except ValueError as e:
+    print(e)
+
+try:
+    ContactList.add_contact("Анна", "123a567890")
+except ValueError as e:
+    print(e)
+
+print("\nСписок всех контактов:")
+for contact in ContactList.all_contacts:
+    print(f"Имя: {contact.name}, Телефон: {contact.phone_number}")
+
+print(ContactList.all_contacts) # []
+
+ContactList.add_contact("Вася Пупкин", "0700100200")
+ContactList.add_contact("Виктор Цой", "0500123456")
+
+for contact in ContactList.all_contacts:
+    print(contact.name, contact.phone)
+    # Вася Пупкин 0700100200
+    # Виктор Цой 0500123456
+
+ContactList.add_contact("John Doe", "5551234") # Error
